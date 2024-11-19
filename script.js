7f3ef9574b588563c58e901fbf56ea8121e04f7f
Er praveenkumar <133311110+Praveen7070kr@users.noreply.github.com>
2024-11-20 00:12:26 +0530

    created a submission

--- script.js
+++
@@ -1,8 +0,0 @@
-describe('tables-in-tables', () => {
-  it('Table 1', () => {
-    cy.visit('path-to-your-file.html');  // Adjust the path
-
-    // Ensure 'Nested Table 2' is found
-    cy.contains('th', 'Nested Table 2').should('be.visible');
-  });
-});
